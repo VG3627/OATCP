951f4c141e5f3404e408bc0e19cf987b2ce7ec5a
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-03-12 16:47:28 +0530

    Add files via upload
    Assignment 1

---
+++ Dice_problem/source.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define vi     vector <int>
+#define vll    vector <long long>
+#define vs     vector <string>
+#define vc     vector <char>
+#define PII    pair<int,int>
+#define mll    map<long long,long long>
+#define UMII   unordered_map<int,int>
+#define sz(a)  a.size()
+#define all(a) a.begin(),a.end()
+#define ff     first 
+#define ss     second 
+#define endl  "\n"
+using namespace std;
+int M = 1e9 + 7 ;
+
+int dp[100001] ;
+int f(int n)
+{
+    if(n < 0)
+    {
+        return -1 ;
+    }
+    if(n == 0)
+    {
+        return 1 ;
+    }
+    if(dp[n] != -1)
+    {
+        return dp[n] ;
+    }
+    int ans = 0 ;
+    for(int i=1;i<=6;i++)
+    {
+        if(i <= n)
+        {
+            ans += (f(n-i) % M) ;
+            ans %= M ;
+        }
+    }
+    return dp[n] = ans ;
+}
+void solve()
+{
+    int n ; cin>>n ;
+    memset(dp,-1,sizeof(dp)) ;
+    if(n == 0)
+    {
+        cout<<-1<<"\n" ; return ; // -1 indicates we can not form the sum using any number of dice rolls. 
+    }
+    
+    int ans = f(n) % M ;
+    cout<<ans<<"\n" ;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    #ifndef CPH 
+        freopen("input.txt","r",stdin) ;
+        freopen("output.txt","w",stdout) ;
+    #endif
+    int t ;
+    cin>>t ;
+    while(t--)
+    {
+        solve() ;
+    }   
+    return 0;
+}
